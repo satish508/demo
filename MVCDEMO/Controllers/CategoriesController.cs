8aa787031ffbaf3ff3f7acf511c2ca1eb874c8aa
SatishChowdary Bolla <bollasatishchowdary@hotmail.com>
2019-01-18 22:26:00 +0530

    master created

---
+++ MVCDEMO/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDEMO.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Home()
+        {
+            return "Home Page";
+
+        }
+
+        public string About()
+        {
+            return "About Page";
+        }
+    }
+}
